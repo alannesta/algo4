f1b76916360d184d8cdabbfd78d5cedf4acc27b5
Alan Cao <alannesta@sina.com>
2016-04-14 16:39:41 -0400

    comments and notes for selection sort

--- src/com/alan/algorithms/chap2/SelectionSort.java
+++ src/com/alan/algorithms/chap2/SelectionSort.java
@@ -12,6 +12,7 @@ public class SelectionSort {
 	public static void sort(Comparable[] a) {
 		int N = a.length;
 
+		// stupid brute force, unneccessary exchange operations
 //		for (int i = 0; i < N-1; i++) {
 //			for (int j = i+1; j<N; j++) {
 //				if (less(a[j], a[i])) {
@@ -20,14 +21,27 @@ public class SelectionSort {
 //			}
 //		}
 
-		// faster implementation from textbook
-		for (int i = 0; i < N; i++) {
+		// reduce exchange operations (array access)
+		for (int i = 0; i < N-1; i++) {
 			int min = i;
-			for (int j = i+1; j < N; j++) {
-				if (less(a[j], a[min])) min = j;
+			for (int j = i+1; j<N; j++) {
+				if (less(a[j], a[min])) {
+					min = j;
+				}
 			}
 			exchange(a, i, min);
 		}
+
+		// faster implementation from textbook
+//		for (int i = 0; i < N; i++) {
+//			int min = i;
+//			for (int j = i+1; j < N; j++) {
+//				if (less(a[j], a[min])) {
+//					min = j;
+//				}
+//			}
+//			exchange(a, i, min);
+//		}
 	}
 
 	private static boolean less(Comparable a, Comparable b) {
