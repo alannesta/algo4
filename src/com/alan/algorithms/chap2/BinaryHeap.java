eea2eccc65e881b9e052cb8f82339ec7d2235bbd
Alan Cao <alannesta@sina.com>
2016-04-22 16:47:07 -0400

    HeapSort

--- src/com/alan/algorithms/chap2/BinaryHeap.java
+++ src/com/alan/algorithms/chap2/BinaryHeap.java
@@ -39,7 +39,25 @@ public class BinaryHeap {
 	private void sink(int index) {
 		while ((2 * index) <= count && (arr[index] < arr[index * 2] || arr[index] < arr[index * 2 + 1])) {
 			if (arr[index] < arr[index * 2]) {
-				if (arr[index] < arr[index * 2 + 1] && arr[index*2] < arr[index*2+1]) {
+				if (arr[index*2] < arr[index*2+1]) {
+					swap(index, index * 2 + 1);
+					index = 2 * index + 1;
+				}else {
+					swap(index, index * 2);
+					index = index * 2;
+				}
+			} else {
+				swap(index, index * 2 + 1);
+				index = 2 * index + 1;
+			}
+		}
+	}
+
+	// heapify
+	private void sink(int[] arr, int index, int count) {
+		while ((2 * index + 1) < count && (arr[index] < arr[index * 2] || arr[index] < arr[index * 2 + 1])) {
+			if (arr[index] < arr[index * 2]) {
+				if (arr[index*2] < arr[index*2+1]) {
 					swap(index, index * 2 + 1);
 					index = 2 * index + 1;
 				}else {
@@ -59,14 +77,47 @@ public class BinaryHeap {
 		arr[j] = temp;
 	}
 
-	public static void main(String[] args) {
-		BinaryHeap bh = new BinaryHeap(15);
-		for (int i = 0; i < 10; i++) {
-			bh.insert(i);
+	private void swap(int[] arr, int i, int j) {
+		int temp = arr[i];
+		arr[i] = arr[j];
+		arr[j] = temp;
+	}
+
+	/**
+	 * build a heap out of an array
+	 * @param arr
+	 */
+	public void buildHeap(int[] arr) {
+		int N = arr.length-1;
+		for (int i = N/2; i > 0; i--) {
+			sink(arr, i, N);
 		}
-		StdOut.println(bh.delMax());
-		StdOut.println(bh.delMax());
-		StdOut.println(bh.delMax());
-		StdOut.println(bh.delMax());
+	}
+
+	public void heapSort(int[] arr) {
+		buildHeap(arr);
+		int N = arr.length-1;
+		for (int i = N-1; i > 0; i--) {
+			swap(arr, i, 1);
+			sink(arr, 1, --N);
+		}
+	}
+
+	public static void main(String[] args) {
+//		BinaryHeap bh = new BinaryHeap(15);
+//		for (int i = 0; i < 10; i++) {
+//			bh.insert(i);
+//		}
+//		StdOut.println(bh.delMax());
+//		StdOut.println(bh.delMax());
+//		StdOut.println(bh.delMax());
+//		StdOut.println(bh.delMax());
+
+		BinaryHeap bh = new BinaryHeap(15);
+		int[] sortable = {0, 6,8,10,11,22, 1, 4, 7};
+		bh.heapSort(sortable);
+		for (int i : sortable) {
+			StdOut.print(i + ",");
+		}
 	}
 }
