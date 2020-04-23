080059510bc411ea363366f5a76db2fe69847949
alannesta <alannesta@sina.com>
2020-04-23 11:36:55 -0400

    bst api

--- src/python/data_structure/bst.py
+++ src/python/data_structure/bst.py
@@ -1,6 +1,7 @@
 """
 Binary search tree impl
 """
+from collections import deque
 
 
 class Node:
@@ -10,12 +11,12 @@ class Node:
         self.right = right
 
     def __str__(self):
-        return 'root: {}, left child: {}, right child: {}'.format(self.value, self.left and self.left.value,
+        return 'value: {}, left child: {}, right child: {}'.format(self.value, self.left and self.left.value,
                                                                   self.right and self.right.value)
 
 
 class BST:
-    def __init__(self, root):
+    def __init__(self, root=Node()):
         self.root = root
 
     def search(self, root, value):
@@ -45,7 +46,6 @@ class BST:
 
         return root
 
-
     def search_range(self, root, min, max, result):
         if not root:
             return result
@@ -66,4 +66,109 @@ class BST:
         if root.value >= max:
             self.search_range(root.left, min, max, result)
 
+    def traversal_inorder(self, root):
+        if not root:
+            return None
+        self.traversal_inorder(root.left)
+        print(root.value)
+        self.traversal_inorder(root.right)
 
+    def traversal_inorder_non_recursive(self, root):
+        stack = deque()
+        stack.appendleft(root)
+
+        visited = []
+        while True:
+            try:
+                current = stack.popleft()
+            except IndexError:
+                break
+
+            if not current.left or current.left in visited:
+                print(current)
+                visited.append(current)
+                continue
+
+            if current.right:
+                stack.appendleft(current.right)
+
+            stack.appendleft(current)
+
+            if current.left:
+                stack.appendleft(current.left)
+
+    def from_ordered_list(self, root, o_list):
+        m_index = self._find_median_index(o_list)
+
+        root.value = o_list[m_index]
+
+        l_list = o_list[0:m_index]
+        if l_list:
+            root.left = self.from_ordered_list(Node(), l_list)
+
+        r_list = o_list[m_index + 1:]
+        if r_list:
+            root.right = self.from_ordered_list(Node(), r_list)
+
+        return root
+
+    def _find_median_index(self, list):
+        # int(3/2) =1
+        return int(len(list) / 2)
+
+    def pretty_print(self):
+        """
+        BFS
+        """
+        tree_height = self.height(self.root)
+
+        bfs_queue = deque()
+        print_queue = deque()
+
+        bfs_queue.appendleft({'node': self.root, 'level': 0, 'parent': None})
+
+        while True:
+            current = None
+            try:
+                current = bfs_queue.pop()
+            except IndexError:
+                break
+
+            print_queue.append(current)
+
+            cur_node = current.get('node')
+            cur_level = current.get('level')
+
+            if cur_node.left:
+                bfs_queue.appendleft({'node': cur_node.left, 'level': cur_level + 1, 'parent': cur_node})
+
+            if cur_node.right:
+                bfs_queue.appendleft({'node': cur_node.right, 'level': cur_level + 1, 'parent': cur_node})
+
+        for info in print_queue:
+            print(info.get('node').value, info.get('level'))
+
+        # TODO: not sure how to calculate horizonal position
+        # root_ident = '\t' * tree_height
+        # _str = ''
+        #
+        # while True:
+        #     current = None
+        #     try:
+        #         current = print_queue.popleft()
+        #     except IndexError:
+        #         break
+        #
+        #     cur_node = current.get('node')
+        #     cur_level = current.get('level')
+
+    def height(self, root):
+        """
+        get height
+        :return: height of the bst
+        """
+
+        if not root:
+            return 0
+
+        return max(self.height(root.left) + 1, self.height(root.right) + 1)
--- src/python/data_structure/bst_test.py
+++ src/python/data_structure/bst_test.py
@@ -38,11 +38,50 @@ ma_tree.add(root, Node(4))
 ma_tree.add(root, Node(6))
 ma_tree.add(root, Node(10))
 ma_tree.add(root, Node(1))
-ma_tree.add(root, Node(5))
+# ma_tree.add(root, Node(0))
+# ma_tree.add(root, Node(5))
 
-print(ma_tree.search(ma_tree.root, 4))
 
-result = []
-ma_tree.search_range(ma_tree.root, 1, 10, result)
+#######
+#search
+#######
+# print(ma_tree.search(ma_tree.root, 4))
 
-print(result)
+#######
+#range search
+#######
+# result = []
+# ma_tree.search_range(ma_tree.root, 1, 10, result)
+# print(result)
+
+
+#######
+#in order traversal
+#######
+# ma_tree.traversal_inorder(ma_tree.root)
+# ma_tree.traversal_inorder_non_recursive(ma_tree.root)
+
+
+##################
+# get height of tree
+##################
+# print(ma_tree.height(ma_tree.root))
+
+##################
+# pretty print
+##################
+# print(ma_tree.pretty_print())
+
+
+##################
+# from ordered list
+##################
+
+test_tree = BST()
+
+ordered_list = [1, 3, 4, 5, 6, 7, 9, 10, 12, 20]
+# print(test_tree.root)
+
+root = test_tree.from_ordered_list(test_tree.root, ordered_list)
+
+test_tree.pretty_print()
