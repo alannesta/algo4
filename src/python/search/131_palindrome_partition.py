46ef662f4f06ffb64b82f919426cd8f3f394d935
alannesta <alannestacao@gmail.com>
2022-10-20 19:11:27 -0400

    131 v2, easier to understand

--- src/python/search/131_palindrome_partition.py
+++ src/python/search/131_palindrome_partition.py
@@ -11,27 +11,26 @@ class Solution:
         self.input = []
 
     def partition(self, s: str) -> List[List[str]]:
-        self.traverse(s, 0, cur_partition=[])
+        self.traverse(s, 0, "", cur_partition=[])
         return self.result
 
-    def traverse(self, s, start_idx, cur_partition):
+    def traverse(self, s, start_idx, cur_str, cur_partition):
         # reached end of input
         if start_idx == len(s):
             self.result.append(cur_partition[::])
+            # cur_partition.pop()
             return
 
         for i in range(start_idx, len(s)):
-            if self.is_valid(s[start_idx: i + 1]):
-                cur_partition.append(s[start_idx: i + 1])
-            else:
-                continue
-            self.traverse(s, i + 1, cur_partition)
-            cur_partition.pop()
+            cur_str += s[i]
+            if self.is_valid(cur_str):
+                cur_partition.append(cur_str)
+                self.traverse(s, i + 1, "", cur_partition)
+                cur_partition.pop()
 
     def is_valid(self, str):
         if not str:
             return False
-
         if len(str) == 1:
             return True
 
