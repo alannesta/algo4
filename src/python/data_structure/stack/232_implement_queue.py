b232068733bb965d597c538e01cd1b03b2b5d327
alannesta <alannestacao@gmail.com>
2022-10-25 13:44:52 -0400

    implement stack using queue

--- src/python/data_structure/stack/232_implement_queue.py
+++ src/python/data_structure/stack/232_implement_queue.py
@@ -33,9 +33,54 @@ class MyQueue:
     def empty(self) -> bool:
         return len(self.in_stack) == 0 and len(self.out_stack) == 0
 
-# Your MyQueue object will be instantiated and called as such:
-# obj = MyQueue()
-# obj.push(x)
-# param_2 = obj.pop()
-# param_3 = obj.peek()
-# param_4 = obj.empty()
+
+from collections import deque
+
+
+class MyStack:
+
+    def __init__(self):
+        self.in_queue = deque()
+        self.out_queue = deque()
+
+    def push(self, x: int) -> None:
+        self.in_queue.appendleft(x)
+
+    def pop(self) -> int:
+        result = None
+        if self.in_queue:
+            while self.in_queue:
+                poped = self.in_queue.pop()
+                if len(self.in_queue) == 0:
+                    result = poped
+                else:
+                    self.out_queue.appendleft(poped)
+
+        else:
+            while self.out_queue:
+                poped = self.out_queue.pop()
+                if len(self.out_queue) == 0:
+                    result = poped
+                else:
+                    self.in_queue.appendleft(poped)
+
+        return result
+
+    def top(self) -> int:
+        res = self.pop()
+        self.in_queue.appendleft(res)
+        return res
+
+    def empty(self) -> bool:
+        return (not self.in_queue) and (not self.out_queue)
+
+
+# Your MyStack object will be instantiated and called as such:
+obj = MyStack()
+obj.push(1)
+obj.push(2)
+obj.push(3)
+print(obj.top())
+print(obj.pop())
+print(obj.top())
+print(obj.pop())
